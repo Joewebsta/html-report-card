a4ac29fd82f1cfe19fbc3deb16e34b4f37c0d852
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-15 14:21:15 -0400

    Click button to insert student name

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ let gradeSpans = document.querySelectorAll('.grade');
 let changeNameBtn = document.querySelector('.change-name-btn');
 let changeGradeBtn = document.querySelector('.change-grades-btn');
 
+function changeName() {
+   studentSpans.forEach(student => student.innerText = 'Joe Webster');
+}
+
+changeNameBtn.addEventListener('click', changeName);
 
